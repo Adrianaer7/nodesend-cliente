4f44e05b4f2799dd42fc7b91ad8f1f455913346c
Adrianaer7 <adrianaer7@hotmail.com>
2021-11-08 22:55:30 -0300

    cuarto

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   env: {
-    backendURL: "http://localhost:4000",  
-    frontendURL: "http://localhost:3000"  //Colocar aqui las 2 url permite tener por ej una url localhost:3000/asd2g-2
+    backendURL: "https://merntask-servidorr.herokuapp.com",  
+    frontendURL: "https://nodesend-cliente-tawny.vercel.app"
   }
 }
